175855acd2d30a194ac2574c52fd077594349db8
sarangi-ankit <sarangiankit097@gmail.com>
2021-08-25 08:51:14 +0530

    add random pic

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-function fun(e)
-{
-var container = document.getElementById('container');
-var val = e.value;
-container.setAttribute("style", "filter: brightness("+val+"%);");
-}
+const input = document.querySelector("#file");
+const brightnessRange = document.querySelector("#brightness-range");
+const container = document.getElementById("container"); 
+
+input.addEventListener("change", () => {
+    const inputFile = input.files[0];
+    // console.log(input.value)
+
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      const base64String = reader.result.replace('data:', '').replace(/^.+,/, '');
+      localStorage.setItem('wallpaper', base64String);
+      container.style.background = `url(data:image/png;base64,${base64String})`;
+    };
+    reader.readAsDataURL(inputFile);
+});
+
+brightnessRange.addEventListener("change",(e)=>{
+    let val = brightnessRange.value
+    container.style.filter = `brightness(${val}%)`
+
+})
